016dc557ae00ef7f456403e1e7592a7df8b5c97e
Jwhited7 <jw504920@ohio.edu>
2021-09-03 17:44:55 -0400

    message

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jacob Whited
+ *        Date: 9/3/21
+ * Description: prints short message
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
